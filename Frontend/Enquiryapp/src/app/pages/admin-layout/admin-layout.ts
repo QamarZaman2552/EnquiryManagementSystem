a638b64c7cdcb451ea1c56aca51a606aac057b15
QAMAR ZAMAN BALOCH <qamarbaloch2023@gmail.com>
2026-07-19 15:09:16 +0500

    fix: sidebar links use routerLink instead of href

--- Frontend/Enquiryapp/src/app/pages/admin-layout/admin-layout.ts
+++ Frontend/Enquiryapp/src/app/pages/admin-layout/admin-layout.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth';
 
 @Component({
   selector: 'app-admin-layout',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './admin-layout.html',
   styleUrl: './admin-layout.css',
 })
